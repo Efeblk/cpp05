1c9a35cd290f1f3758fe5725546c07e91fc1cb16
ismet efe balik <ibalik@k1m06s03.42istanbul.com.tr>
2024-03-04 19:25:32 +0300

    mini adjustment 2

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -50,3 +50,13 @@ std::ostream &operator<<(std::ostream &out, Bureaucrat const &bureaucrat)
     out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
     return out;
 }
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low";
+}
--- ex03/Bureaucrat.cpp
+++ ex03/Bureaucrat.cpp
@@ -75,3 +75,13 @@ void Bureaucrat::executeForm(AForm const &form)
         std::cout << _name << " cannot execute " << form.getName() << " because " << e.what() << std::endl;
     }
 }
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low";
+}
